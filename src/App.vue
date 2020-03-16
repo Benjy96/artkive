746c7b99fc06b3030e1351f88afb1716c380ac48
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2020-03-16 12:23:03 +0000

    Add login page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
   <v-app>
-    <router-view/>
+    <v-app-bar app flat hide-on-scroll>
+      <v-spacer></v-spacer>
+      <v-btn
+        class="mr-2"
+        text
+        to="/Login"
+      >
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+      <v-btn
+        class="mr-2"
+        text
+        to="/"
+      >
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <v-fade-transition>
+          <router-view class="mx-auto"/>
+        </v-fade-transition>
+      </v-container>
+    </v-content>
+    
+    
   </v-app>
 </template>
---
+++ src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-card width="400px">
+        <v-card-title><h1>Login</h1></v-card-title>
+
+        <v-card-text>
+            <v-form ref="form" @submit.prevent="login">
+                <v-text-field v-model="email"
+                required
+                v-bind:rules="emailRules"
+                label="e-mail" prepend-icon="mdi-account-circle"
+                />
+                <v-text-field v-model="password"
+                @click:append="showPassword = !showPassword"
+                v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                v-bind:type="showPassword ? 'text' : 'password'"
+                label="Password" prepend-icon="mdi-lock" 
+                />
+
+                <input type="submit" hidden/>
+            </v-form>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="success" @click="login">Login</v-btn>
+        </v-card-actions>
+
+    </v-card>
+</template>
+
+<script>
+// import firebase from 'firebase';
+
+export default {
+    name: 'Login',
+    data() {
+        return {
+            showPassword: false,
+            email: '',
+            password: '',
+            emailRules: [
+                v => !!v || "Required",
+                v => /.+@.+/.test(v) || "Email is invalid",
+            ]
+        }
+    },
+    methods: {
+        login() {  //event is a MouseEvent - passed in by Javascript
+            if(!this.$refs.form.validate()) return;
+            
+            // let vueInstance = this;
+            // firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function(error) {
+            //     vueInstance.$emit("open-generic-dialog", [vueInstance.$getLanguageMsg("Error"), error.message]);
+            // });
+        }
+    }
+}
+</script>
