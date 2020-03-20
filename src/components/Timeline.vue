048b8e852dcad8ef934e6ce995df0f61df2fb6d8
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2020-03-20 07:01:23 +0000

    Artists can now only edit one picture at a time

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -14,7 +14,7 @@
           <v-timeline-item v-for="(item, index) in timelineItems" :key="index" color="indigo lighten-4">
 
             <!-- Date -->
-            <span v-if="!editMode" slot="opposite" class="headline font-weight-light">{{item.date}}</span>
+            <span v-if="index != artworkBeingEditedIndex" slot="opposite" class="headline font-weight-light">{{item.date}}</span>
             <span v-else slot="opposite" class="headline font-weight-light">
               <DatePicker :initialDate="item.date" v-on:date-saved="item.date = $event"></DatePicker>
             </span>
@@ -22,24 +22,24 @@
             <v-card class="elevation-4">
 
               <!-- Title & Edit Button -->
-              <v-card-title class="display-1 font-weight-light" v-if="!editMode">
-                <v-btn v-if="$root.user" @click="editMode = true" icon class="mr-2">
+              <v-card-title class="display-1 font-weight-light" v-if="index != artworkBeingEditedIndex">
+                <v-btn v-if="$root.user" @click="enableEditing(index)" icon class="mr-2">
                   <v-icon>mdi-pencil</v-icon>
                 </v-btn>
                 {{item.title}}
               </v-card-title>
 
               <v-card-title class="display-1 font-weight-light" v-else>
-                <v-btn v-if="$root.user && editMode" @click="editMode = false" icon class="mr-2">
+                <v-btn v-if="$root.user && index == artworkBeingEditedIndex" @click="cancelEditing" icon class="mr-2">
                   <v-icon>mdi-close</v-icon>
                 </v-btn>
-                <v-btn v-if="$root.user && editMode" @click="updateArtwork(index)" icon class="mr-2" color="success">
+                <v-btn v-if="$root.user && index == artworkBeingEditedIndex" @click="updateArtwork(index)" icon class="mr-2" color="success">
                   <v-icon>mdi-check</v-icon>
                 </v-btn>
 
                 <v-text-field class="display-1 font-weight-light" v-model="item.title"></v-text-field>
 
-                <v-btn v-if="$root.user && editMode" @click="deleteArtwork(index)" icon class="ml-2" color="error">
+                <v-btn v-if="$root.user && index == artworkBeingEditedIndex" @click="deleteArtwork(index)" icon class="ml-2" color="error">
                   <v-icon>mdi-delete</v-icon>
                 </v-btn>
 
@@ -49,7 +49,7 @@
               <v-img :src="item.image"></v-img>
 
               <!-- Description edit input & description -->
-              <v-card-text v-if="editMode">
+              <v-card-text v-if="index == artworkBeingEditedIndex">
                 <v-textarea clearable v-model="item.description"></v-textarea>
               </v-card-text>
               <v-card-text v-else-if="item.description.length > 0">
@@ -89,7 +89,7 @@ export default {
   },
   data: () => ({
     firstname: null,
-    editMode: false,
+    artworkBeingEditedIndex: -1,
     timelineItems: []
   }),
   methods: {
@@ -97,19 +97,25 @@ export default {
       return require("@/assets/" + src);
     },
     updateArtwork(artworkIndex) {
-      this.editMode = false;
+      this.artworkBeingEditedIndex = -1;
       let artwork = this.timelineItems[artworkIndex];
       FirebaseService.updateArtwork(artwork.id, artwork.title, artwork.description, artwork.date);
     },
     deleteArtwork(artworkIndex) {
       if(confirm("Are you sure you wish to delete this?")) {
-        this.editMode = false;
+        this.artworkBeingEditedIndex = -1;
         let artworkID = this.timelineItems[artworkIndex].id;
 
         this.timelineItems = this.timelineItems.filter(x => x.id != artworkID);
 
         FirebaseService.deleteArtwork(artworkID);
       }
+    },
+    enableEditing(index) {
+      this.artworkBeingEditedIndex = index;
+    },
+    cancelEditing() {
+      this.artworkBeingEditedIndex = -1;
     }
   }
 }
